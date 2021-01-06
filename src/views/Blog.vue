da70d09f146592efa92706f5b308eb6f37497b62
thejoin95 <miki.lombi@gmail.com>
2021-01-06 16:45:19 +0100

    add articles.json for listing usage

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -3,3 +3,18 @@
     <h1>Blog listing</h1>
   </div>
 </template>
+
+<script>
+const articles = require('../assets/articles.json');
+
+export default {
+  data() {
+    return {
+      articles,
+    };
+  },
+  mounted() {
+    console.log(this.articles);
+  },
+};
+</script>

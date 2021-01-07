84e16ddbbf0a0395760131cfd380366f9e643611
thejoin95 <miki.lombi@gmail.com>
2021-01-07 19:34:00 +0100

    add date in articles.json and put articles in view

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="blog">
     <h1>Blog listing</h1>
+    <div class="blog-list">
+      <div v-for="article in articles" :key="article.sefurl">
+        <div class="article">
+          <h3><router-link :to="`/blog/${article.sefurl}`">{{ article.title }}</router-link></h3>
+          <small>{{ article.date }}</small>
+          <p>{{ article.description }}</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 

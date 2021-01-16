fb92b8ca786e93b9006d826024f07091285f2643
thejoin95 <miki.lombi@gmail.com>
2021-01-16 15:59:13 +0100

    add draft hero in homepage

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="hello box-content h-20 w-32 p-4 border-4 border-gray-400 bg-gray-200 mx-auto">
-    <div class="h-full w-full bg-gray-400">
-      <h1 class="text-scramble">I'm Miki Lombardi</h1>
-      <h2 class="text-scramble">A passion driven developer</h2>
+  <div class="hello m-10">
+    <div class="p-5">
+      <h1 class="font-bold text-6xl mt-5 text-scramble">I'm Miki Lombardi</h1>
+      <h2 class="text-4xl text-scramble">A passion driven developer</h2>
     </div>
   </div>
 </template>
@@ -10,26 +10,10 @@
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String,
-  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts}'],
   darkMode: false, // or 'media' or 'class'
-  theme: {},
+  theme: {
+    inset: {
+      50: '50%',
+    },
+  },
   variants: {
     extend: {},
   },

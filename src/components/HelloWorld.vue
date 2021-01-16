8dc65aaafef228068f35e208e5fbd7590f6e1fc2
thejoin95 <miki.lombi@gmail.com>
2021-01-16 16:16:41 +0100

    add responsive margins in hello component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,15 @@
 <template>
-  <div class="hello m-10">
+  <div class="hello sm:m-10">
     <div class="p-5">
-      <h1 class="font-bold text-6xl mt-5 text-scramble">I'm Miki Lombardi</h1>
-      <h2 class="text-4xl text-scramble">A passion driven developer</h2>
+      <div class="grid grid-cols-6 gap-4">
+        <div class="col-start-2 col-span-4 h-20"></div>
+        <div class="col-start-1 col-end-7">
+          <h1 class="font-bold text-6xl mt-5 mb-1 text-scramble">I'm Miki Lombardi</h1>
+        </div>
+        <div class="col-start-1 col-end-7">
+          <h2 class="text-4xl mt-1 text-scramble">A passion driven developer</h2>
+        </div>
+      </div>
     </div>
   </div>
 </template>

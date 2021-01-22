18370701cd00f5ec846346f9ddbc8e8a5e48932e
thejoin95 <miki.lombi@gmail.com>
2021-01-22 19:28:04 +0100

    add style and fix markdown header

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -1,6 +1,6 @@
 <template>
-  <section class="article container mx-auto m-width-780">
-    <article class="article-content px-3 prose prose-xl" v-html="article"></article>
+  <section class="article container mx-auto m-width-780 px-5">
+    <article class="article-content p-3 prose prose-xl" v-html="article"></article>
   </section>
 </template>
 
@@ -27,12 +27,13 @@ export default {
       fetch(`/articles/${this.$route.params.article}.md`)
         .then(async (r) => {
           console.log(r.status);
-          const text = await r.text();
+          let text = await r.text();
           // Workaround, da modificare in produzione con redirect
           if (text.search(/sorry/) !== -1) {
             self.redirectTo404();
           }
 
+          text = text.substr(text.substr(3).search('---') + 7);
           self.article = marked(text);
         }, () => {
           self.redirectTo404();
@@ -44,3 +45,12 @@ export default {
   },
 };
 </script>
+<style lang="scss" scoped>
+.article {
+  background: $dark-4;
+
+  &-content {
+    background: $light-1;
+  }
+}
+</style>

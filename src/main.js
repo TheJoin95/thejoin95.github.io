96dbda89d08a379ffde2a4113894faebdc67ae4d
thejoin95 <miki.lombi@gmail.com>
2021-01-30 00:21:01 +0100

    add gtag autotracking

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue';
+import VueGtag from 'vue-gtag';
 import VueMeta from 'vue-meta';
 import App from './App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueGtag, {
+  config: { id: 'UA-98330359-1' },
+}, router);
 
 Vue.use(VueMeta, {
   refreshOnceOnNavigation: true,

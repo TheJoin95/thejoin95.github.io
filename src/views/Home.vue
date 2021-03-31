d9ecd81dd8a60a9c4d275ad9c157dcd3f8cde6ed
thejoin95 <miki.lombi@gmail.com>
2021-03-31 18:04:31 +0200

    add sidebar and new sections in home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,28 +6,83 @@
     <div class="h-8 relative">
       <NyanCat />
     </div>
-    <div class="container mx-auto p-5 md:p-0 md:py-5">
-      <div class="m-0 md:m-8">
-        <h1 class="text-3xl">TLDR;</h1>
-        <div class="prose md:prose-xl text-justify">
-          <p class="biography">
-            I'm a passion & creative driven developer who has ton of others
-            passion: <i>basketball, cycling, photography</i> are few of them.
-          </p>
-          <p class="biography">
-            Since <b>2014</b> I have lent my expertise in the <b>software develpoment</b>
-            and <b>solution architect</b> fields in <b>Florence, Italy</b>. <br/>
-            I've been working as a software engineer & solution architect
-            for <b>7+ years</b> in both product and
-            consultancy companies, taking the best from both worlds.<br/>
-            I'm currently working as a <b>Head of Research & Development</b> divison at
-            <a href="https://www.plansoft.it" target="_blank">Plansoft</a>, where I'm innovating
-            the tech stack of the company.
-            <br/><br/>
-            Are you interested on reading more? Go into the
-            <router-link to="/about">WhoAmI</router-link> section
-          </p>
+    <div class="container mx-auto grid grid-cols-6">
+      <div class="col-span-6 md:col-span-5">
+        <div class="p-5 md:p-0 md:py-5">
+          <div class="m-0 md:m-8">
+            <h3 class="text-3xl">TLDR;</h3>
+            <div class="prose md:prose-xl text-justify">
+              <p class="biography">
+                I'm a passion & creative driven developer who has ton of others
+                passion: <i>basketball, cycling, photography</i> are few of them.
+              </p>
+              <p class="biography">
+                Since <b>2014</b> I have lent my expertise in the <b>software develpoment</b>
+                and <b>solution architect</b> fields in <b>Florence, Italy</b>. <br/>
+                I've been working as a software engineer & solution architect
+                for <b>7+ years</b> in both product and
+                consultancy companies, taking the best from both worlds.<br/>
+                I'm currently working as a <b>Head of Research & Development</b> divison at
+                <a href="https://www.plansoft.it" target="_blank">Plansoft</a>, where I'm innovating
+                the tech stack of the company.
+                <br/><br/>
+                Are you interested on reading more? Go into the
+                <router-link to="/about">WhoAmI</router-link> section
+              </p>
+            </div>
+          </div>
         </div>
+        <div class="container mx-auto p-5 md:p-0 md:py-5">
+          <div class="m-0 md:m-8">
+            <h1 class="text-3xl">Open source projects</h1>
+            <div class="max-w-sm rounded overflow-hidden shadow-lg bg-gray-700">
+              <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2">Covid Supermarket Waiting Times</div>
+                <p class="text-base">
+                  Stuff I made on the project
+                </p>
+              </div>
+              <div class="px-6 pt-4 pb-2">
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1
+                text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1
+                text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1
+                text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="container mx-auto p-5 md:p-0 md:py-5">
+          <div class="m-0 md:m-8">
+            <h1 class="text-3xl">I've been featured on</h1>
+            <div class="max-w-sm rounded overflow-hidden shadow-lg bg-gray-700">
+              <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2">Blog.to</div>
+                <p class="text-base">
+                  Supermarket waiting times project
+                </p>
+              </div>
+              <div class="px-6 pt-4 pb-2">
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1
+                text-sm font-semibold text-gray-700 mr-2 mb-2">#canada</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1
+                text-sm font-semibold text-gray-700 mr-2 mb-2">#english</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1
+                text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sm:hide col-span-1 pt-5 text-center">
+        <h3 class="text-xl mt-10"><i class="bg-grey-200">Brief</i></h3>
+        <div class="circle mx-auto" style="
+        visibility: inherit; opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0);">
+        </div>
+        <div class="spine mx-auto h-full" style="
+        visibility: inherit; opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0);"
+        ></div>
       </div>
     </div>
   </section>
